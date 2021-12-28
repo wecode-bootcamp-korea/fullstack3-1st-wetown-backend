1cb8b506850ba22aecc613869096c8e0e292fb04
LJHyeokk <clzlsqkq56@naver.com>
2021-12-28 15:15:50 +0900

    [추가] - cat.sql 10개

--- dummy_sql/01_insert_categories_users.sql
+++ dummy_sql/01_insert_categories_users.sql
@@ -1,7 +1,7 @@
 INSERT INTO categories(name) VALUES("dog"),("cat"),("turtle"),("hamster"),("bird");
 
-INSERT INTO subcategories(name) VALUES("food","toy","house","health");
+  INSERT INTO subcategories(name) VALUES("food"),("toy"),("house"),("health");
 
 INSERT INTO users(name, gender, phone_number, nickname, password, email) VALUES("관리자","중성","010-1234-5678","admin","1234","admin@wetown.com");
-
+W
 INSERT INTO policies(is_essential_agreed,is_optional_agreed,user_id) VALUES(true, true, 1);
