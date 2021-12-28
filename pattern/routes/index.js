ce29dd0d526dbbb087fbe9a3431d5847854ace58
heeyun <ato8757@gmail.com>
2021-12-28 09:50:13 +0900

    [초기세팅] - 초기세팅

--- pattern/routes/index.js
+++ pattern/routes/index.js
@@ -1,1 +1,12 @@
-//asd
+const express = require('express');
+const router = express.Router();
+
+const productRouter = require('./productRouter');
+const userRouter = require('./userRouter');
+
+console.log('routes');
+
+router.use('/products', productRouter);
+router.use('/users', userRouter);
+
+module.exports = router;
