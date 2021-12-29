6ee4a006136a582eca6c26f507933a9eedc08ffe
heeyun <ato8757@gmail.com>
2021-12-29 15:14:47 +0900

    [추가] 모델단 createUser 만듬

--- models/userDao.js
+++ models/userDao.js
@@ -3,8 +3,21 @@ import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 
 //회원가입
-const signUp = async () => {};
+const createUser = async (
+  name,
+  gender,
+  phone_number,
+  nickname,
+  password,
+  email
+) => {
+  console.log('createUser :', name, phone_number, password);
+  await prisma.$queryRaw`
+  INSERT INTO users(name, gender, phone_number, nickname, password, email) 
+  VALUES("관리자","중성","010-1234-5678","admin","1234","admin@wetown.com");
+  `;
+};
 //로그인
 const signIn = async () => {};
 
-export default { signUp, signIn };
+export default { createUser, signIn };
