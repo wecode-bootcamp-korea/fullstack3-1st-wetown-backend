58f7353109f8547194df047996f877fffa0dbafd
heeyun <ato8757@gmail.com>
2021-12-29 14:49:22 +0900

    [추가] 서비스와 모델단 회원가입

--- services/userServices.js
+++ services/userServices.js
@@ -1,7 +1,32 @@
 import { userDao } from '../models';
 
+import bcrypt from 'bcryptjs'; // 단방향 암호화
+import jwt from 'jsonwebtoken';
+
+const makeHash = async password => {
+  return await bcrypt.hash(password, 10);
+};
 //회원가입
-const signUp = async () => {};
+const signUp = async (
+  name,
+  gender,
+  phone_number,
+  nickname,
+  password,
+  email
+) => {
+  console.log('userService user:', name, phone_number, password);
+  if (user) {
+    throw (error = new Error('이미 가입된 이메일입니다'));
+  }
+
+  const hashedPW = await makeHash(password);
+
+  console.log('hashedPW', hashedPW);
+
+  return await userModel.createUser(email, hashedPW);
+};
+
 // 로그인
 const signIn = async () => {};
 
