62a005a2e29856794f5b72b1bfab72b13aafd68d
TaeJoonPark <tjpark1028@gmail.com>
2022-01-03 17:58:03 +0900

    [수정] 1.MVC/index.js 파일에 cart mvc 파일 import 및 각 cart mvc 파일에 index파일 import

--- services/index.js
+++ services/index.js
@@ -1,5 +1,11 @@
 import productFilterServices from './productFilterServices';
 import userServices from './userServices';
 import productDetailServices from './productDetailServices';
+import cartService from './cartService';
 
-export { productFilterServices, userServices, productDetailServices };
+export {
+  productFilterServices,
+  userServices,
+  productDetailServices,
+  cartService,
+};
